c6401402897306fec850a28bbfc636629df809b7
Victor Huallpa <vech.600@gmail.com>
2026-03-17 15:26:19 -0500

    cabecera menu

---
+++ Menu.h
@@ -0,0 +1,31 @@
+#ifndef MENU_H
+#define MENU_H  
+
+class Menu
+{
+private:
+    //atributos
+    int opcion;
+public:
+    //contructor
+    Menu();
+    //destructor
+    // ~Menu();
+
+    //metodos
+    void mostrarMenu();
+
+
+};
+
+Menu::Menu(/* args */)
+{
+}
+
+Menu::~Menu()
+{
+}
+
+
+
+#endif
