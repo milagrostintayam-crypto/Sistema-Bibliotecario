666c72697d42418a8f9f84ec4b37a36766be00a2
Victor Huallpa <vech.600@gmail.com>
2026-03-18 23:50:05 -0500

    Construccion de metodos de la clase Usuario

--- Usuario.cpp
+++ Usuario.cpp
@@ -3,3 +3,20 @@
 
 using namespace std;
 
+Usuario::Usuario(string name, string dni, int phone, string direction){
+    this->name = name;
+    this->dni= dni;
+    this->phone = phone;
+    this->direction = direction;
+}
+
+void Usuario::mostrarUsuario(){
+    cout<<"Nombre: "<<name<<endl;
+    cout<<"DNI: "<<dni<<endl;
+    cout<<"Celular: "<<phone<<endl;
+    cout<<"Direccion: "<<direction<<endl;
+}
+
+string Usuario::getDni(){
+    return dni;
+}
