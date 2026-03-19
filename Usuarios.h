c176ad017784f91e0a266a8fff24f91b9183a672
Victor Huallpa <vech.600@gmail.com>
2026-03-18 23:52:14 -0500

    Definiendo nuevos metodos y atributos

--- Usuarios.h
+++ Usuarios.h
@@ -1,12 +1,26 @@
 #ifndef USUARIOS_H
 #define USUARIOS_H
 
+#include "Usuario.h"
+
 class Usuarios
 {
 private:
+    /* Puntero */
+    Usuario** lista;
     /* Atributos */
+    int cantidad;
+    int capacidad;
+
+    void redimensionar();
 public:
     /* Metodos */
+    Usuarios(int cap);
+    ~Usuarios();
+
+    void mostrarUsuarios();
+    void agregarUsuario(Usuario* u);
+    Usuario* buscarUsuario(string dni) ;
 
 };
 
