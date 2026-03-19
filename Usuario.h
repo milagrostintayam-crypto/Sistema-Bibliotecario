bbb942ad0997741c6a8ad7d58b5b9323112843ad
Victor Huallpa <vech.600@gmail.com>
2026-03-18 23:51:08 -0500

    Definiendo nuemovos metodos y atributos

--- Usuario.h
+++ Usuario.h
@@ -1,13 +1,23 @@
 #ifndef USUARIO_H
 #define USUARIO_H
+#include <string>
+
+using namespace std;
 
 class Usuario
 {
 private:
     /* Atributos */
+    string name;
+    string dni;
+    int phone;
+    string direction;
 public:
     /* Metodos */
+    Usuario(string name, string dni, int phone, string direction);
 
+    void mostrarUsuario();
+    string getDni();
 };
 
 
