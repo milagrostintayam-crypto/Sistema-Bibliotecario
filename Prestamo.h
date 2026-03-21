ad9109baf98d785a13378bca382ee1bfeac2157a
Victor Huallpa <vech.600@gmail.com>
2026-03-20 20:54:54 -0500

    Definiendo los metodos y atributos

---
+++ Prestamo.h
@@ -0,0 +1,28 @@
+#ifndef PRESTAMO_H
+#define PRESTAMO_H
+#include <string>
+
+class Prestamo {
+private:
+    std::string fechaPrestamo;
+    std::string fechaDevolucion;
+    std::string libroId;
+    std::string usuarioId;
+public:
+    Prestamo(std::string fechaPrestamo, std::string fechaDevolucion, std::string libroId, std::string usuarioId);
+    ~Prestamo();
+
+    void mostrarPrestamo() const;
+
+    std::string getFechaPrestamo() const;
+
+    std::string getFechaDevolucion() const;
+
+    std::string getLibro() const;
+
+    std::string getUsuario() const;
+
+
+};
+
+#endif // PRESTAMO_H
